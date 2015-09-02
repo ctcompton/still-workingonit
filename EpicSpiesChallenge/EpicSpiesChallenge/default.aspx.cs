c244b510f9c009c4a4de00f9f22d27bba82eefc5
ctcompton <ct.compton@yahoo.com>
2015-09-02 15:14:48 -0500

    Epic Spies Challenge ch2

--- EpicSpiesChallenge/EpicSpiesChallenge/default.aspx.cs
+++ EpicSpiesChallenge/EpicSpiesChallenge/default.aspx.cs
@@ -21,36 +21,45 @@ namespace EpicSpiesChallenge
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            // set variables 
             DateTime value1 = previousCalendar.SelectedDate;
             DateTime value2 = startCalendar.SelectedDate;
             DateTime value3 = endCalendar.SelectedDate;
-            resultLabel.Text = " ";
+            
             string a = nameTextBox.Text;
             string b = assignmentTextBox.Text;
-
+            
+            // make equations
             TimeSpan offtime = value2 - value1;
             TimeSpan work = value3 - value2;
 
             double work1 = work.Days;
             double cost = work1 * 500;
 
-
-            if (offtime.TotalDays < 14)
-                    resultLabel.Text = "error: You must have 14 days between assignments";
+            
+            
 
             
             if (value2 > value3)
                 work = value2 - value3;
             
-            if (work1 > 21)
+            if (work1 >= 21)
             {
                 cost += 1000;
             }
 
+            
             string result = String.Format("Assignment of {0}" +
                 " to assignment Project: {1} is authorized." +
                " Budget total: {2:C}", a, b, cost);
+
+            if (offtime.TotalDays < 14)
+                resultLabel.Text = " ERROR: You must have 14 days between assignments";
+
+            else
             resultLabel.Text = result;
+
+
                 
         
                 
