5b5fbe8a11722a62b2893f383a941c2c2f63a2e3
ctcompton <ct.compton@yahoo.com>
2015-09-02 01:10:55 -0500

    epic spy challenge incomplete

--- EpicSpiesChallenge/EpicSpiesChallenge/default.aspx.cs
+++ EpicSpiesChallenge/EpicSpiesChallenge/default.aspx.cs
@@ -11,7 +11,7 @@ namespace EpicSpiesChallenge
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            previousCalendar.SelectedDate = DateTime.Now;
+            previousCalendar.SelectedDate = DateTime.Now.Date;
             startCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
             endCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
             
@@ -19,23 +19,31 @@ namespace EpicSpiesChallenge
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            DateTime value1 = startCalendar.SelectedDate;
-            DateTime value2 = previousCalendar.SelectedDate;
+            DateTime value1 = previousCalendar.SelectedDate;
+            DateTime value2 = startCalendar.SelectedDate;
             DateTime value3 = endCalendar.SelectedDate;
             
 
-            TimeSpan offtime = value2 - value1;
 
+            TimeSpan offtime = (value2) - (value1);
+            if (value1 > value2)
+            {
+                offtime = (value1) - (value2);
+            }
+                                 
 
-            if (offtime.TotalDays < (14))
+            if (offtime.TotalDays < 13)
             {
                 resultLabel.Text = "error";
             }
-                
-            else if (offtime.TotalDays >= (14))
+            
+
+            else if (offtime.TotalDays >= 14)
             {
                 resultLabel.Text = " ";
             }
+                
+            
                 
         }
     }
