b5801c9d3888fcdf3f9bdc632c1c89912ae9086d
ctcompton <ct.compton@yahoo.com>
2015-09-02 17:49:27 -0500

    CS-ASP_023

---
+++ CS-ASP_023/CS-ASP_023/default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CS_ASP_023._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Hours Worked on Project:&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Add Hours" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_023/CS-ASP_023/default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_023
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                double[] hours = new double[0];
+                ViewState.Add("Hours", hours);
+                
+            }
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            double[] hours = (double[])ViewState["Hours"];
+
+            Array.Resize(ref hours, hours.Length + 1);
+            int newestItem = hours.GetUpperBound(0);
+
+            hours[newestItem] = double.Parse(TextBox1.Text);
+            ViewState["Hours"] = hours;
+            resultLabel.Text = string.Format("Total hours: {0} ", hours.Sum());
+
+        }
+    }
+}
