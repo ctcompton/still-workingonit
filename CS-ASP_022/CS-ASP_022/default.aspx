ec0b08b59a7207872e1d41170f2f0c1e72f71e11
ctcompton <ct.compton@yahoo.com>
2015-09-02 17:15:24 -0500

    CS-ASP_022

---
+++ CS-ASP_022/CS-ASP_022/default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CS_ASP_022._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        From:<br />
+        <br />
+        <asp:RadioButton ID="RadioButton1" runat="server" GroupName="fromcity" Text="Chicago" />
+        <br />
+        <asp:RadioButton ID="RadioButton2" runat="server" GroupName="fromcity" Text="New York" />
+        <br />
+        <asp:RadioButton ID="RadioButton3" runat="server" GroupName="fromcity" Text="London" />
+        <br />
+        <br />
+        To:<br />
+        <br />
+        <asp:RadioButton ID="RadioButton4" runat="server" GroupName="tocity" Text="Chicago" />
+        <br />
+        <asp:RadioButton ID="RadioButton5" runat="server" GroupName="tocity" Text="New York" />
+        <br />
+        <asp:RadioButton ID="RadioButton6" runat="server" GroupName="tocity" Text="London" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" style="height: 26px" Text="OK" />
+        <br />
+        <br />
+        Ticket Price:&nbsp; <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_022/CS-ASP_022/default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_022
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        double[,] pricegrid;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //double[,] pricegrid = new double[3, 3];
+            //0 - chicago
+            //1 - new york
+            //2 - london
+            pricegrid = new double[3, 3];
+
+            pricegrid[0, 1] = 350.0;
+            pricegrid[0, 2] = 750.0;
+            pricegrid[1, 0] = 400.0;
+            pricegrid[1, 2] = 700d;
+            pricegrid[2, 0] = 800d;
+            pricegrid[2, 1] = 805d;
+
+            //resultLabel.Text = pricegrid[1, 2].ToString();
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int fromCity;
+
+            if (RadioButton1.Checked) fromCity = 0;
+            else if (RadioButton2.Checked) fromCity = 1;
+            else fromCity = 2;
+
+            int toCity;
+
+            if (RadioButton4.Checked) toCity = 0;
+            else if (RadioButton5.Checked) toCity = 1;
+            else toCity = 2;
+
+            if (fromCity == toCity)
+            {
+                resultLabel.Text = " ";
+                return;
+            }
+            resultLabel.Text = pricegrid[fromCity, toCity].ToString();
+        }
+    }
+}
