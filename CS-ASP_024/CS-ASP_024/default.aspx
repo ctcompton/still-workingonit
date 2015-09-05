430de4b9ea457eb80492f356e8f83764b5fd33b2
ctcompton <ct.compton@yahoo.com>
2015-09-05 10:53:25 -0500

    cs-asp_024

---
+++ CS-ASP_024/CS-ASP_024/default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CS_ASP_024._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_024/CS-ASP_024/default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_024
+{
+    public partial class _default : System.Web.UI.Page
+
+    {
+        string z = " ";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int x = 1;
+            z = +"bob";
+
+            if (x == 2)
+            {
+                x = 2;
+                //int = y;
+                z += "tabor";
+            }
+
+            //string result = x.ToString();
+            //string result = y.ToString();
+
+        }
+    }
+}
